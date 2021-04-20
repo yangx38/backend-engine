4c1c6746638c374602eb81aeae2e1fcf0ea3f988
Xiaotong Yang <syangx38@gmail.com>
2021-04-20 11:55:45 -0700

    Apr 20: minor format change

--- src/routes/systemadmin.js
+++ src/routes/systemadmin.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
 const router = new Router({prefix:'/api/systemadmin'});
-const { find, findById, create, update, delete: del} = require('../controllers/systemadmin');
+const { find, findById, create, update, delete: del} = require('../controller/systemadmin');
 
 router.get('/', find)
 router.post('/', create)
